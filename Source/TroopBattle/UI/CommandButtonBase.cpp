67e0b66d7cd860f6b4e120564d6ce4bb947c6b4f
pushbell7 <redjm900@gmail.com>
2024-06-23 18:21:12 +0900

    add command execution scafold

--- Source/TroopBattle/UI/CommandButtonBase.cpp
+++ Source/TroopBattle/UI/CommandButtonBase.cpp
@@ -16,6 +16,24 @@ bool UCommandButtonBase::Initialize()
 void UCommandButtonBase::HandleClicked()
 {
 	UE_LOG(LogTemp, Log, TEXT("type : %s"),  EnumToString(CommandType));
+	switch (CommandType)
+	{
+	case ECommandType::Move:
+		// make callback to destination
+		break;
+	case ECommandType::ChangeMovingMethod:
+		// change internal state
+		break;
+	case ECommandType::Stop:
+		// change internal state
+		break;
+	case ECommandType::Observe:
+		// change internal state
+		break;
+	case ECommandType::Hold:
+		// change internal state
+		break;
+	}
 }
 
 void UCommandButtonBase::SetCommandType(ECommandType type)
